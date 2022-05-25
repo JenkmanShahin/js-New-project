617252daa9b38862c765ec3e87e874ed88bf7ea2
jenkman <Jenkmanshahin17@gmail.com>
2022-05-25 16:06:42 +0200

    Lektion3. Aufgabe changes

--- src/App.js
+++ src/App.js
@@ -116,6 +116,7 @@ class App extends React.Component {
                    onKeyPress={e => (e.key == 'Enter') ? this.artikelHinzufuegen() : ''}/>
             <span className="mdc-line-ripple"></span>
             <i className="material-icons mdc-text-field__icon mdc-text-field__icon--trailing"
+               tabIndex="0" role="button"
                onClick={() => this.artikelHinzufuegen()}>add_circle</i>
           </label>
 
